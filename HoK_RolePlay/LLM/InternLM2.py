e3d6fe19e6e9364f5e8a9da8eba3b44fbc8c8581
YongXie66 <1620469296@qq.com>
2024-06-11 17:58:27 +0800

    update

--- HoK_RolePlay/LLM/InternLM2.py
+++ HoK_RolePlay/LLM/InternLM2.py
@@ -10,7 +10,8 @@ class InternLM2:
         self.prefix_prompt = prefix_prompt
         self.mode = mode
         self.model, self.tokenizer = self.init_model(model_path)
-        self.history = None
+        self.model = self.model.eval()
+        self.history = []
     
     def init_model(self, path = "InternLM2/InternLM2_7b"):
         model = AutoModelForCausalLM.from_pretrained(path, 
@@ -22,33 +23,15 @@ class InternLM2:
         return model, tokenizer   
     
     def generate(self, question, system_prompt="You are a helpful assistant."):
-        device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        messages = [
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": self.prefix_prompt + question}
-        ]
-        text = self.tokenizer.apply_chat_template(
-            messages,
-            tokenize=False,
-            add_generation_prompt=True
-        )
-        model_inputs = self.tokenizer([text], return_tensors="pt").to(device)
-        generated_ids = self.model.generate(
-            model_inputs.input_ids,
-            max_new_tokens=512
-        )
-        generated_ids = [
-            output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-        ]
+        response, history = self.model.chat(self.tokenizer, self.prefix_prompt + question, history=[])
 
-        response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-        # print(self.history)
         return response
         
     def chat(self, system_prompt, message, history):
-        response = self.generate(message, system_prompt)
-        history.append((message, response))
-        return response, history
+        response, self.history = self.model.chat(self.tokenizer, message, history=self.history)
+        # history.append((message, response))
+
+        return response, self.history
     
     def clear_history(self):
         # 清空历史记录
